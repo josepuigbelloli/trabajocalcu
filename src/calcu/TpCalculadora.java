329fbd9da0873dff4b42dd5a2f3c682d97687d84
Bren <brendaffunchio@gmail.com>
2020-04-16 19:04:25 -0300

    clase con switch

---
+++ src/calcu/TpCalculadora.java
@@ -0,0 +1,47 @@
+package calcu;
+
+import java.util.Scanner;
+
+public class TpCalculadora {
+
+	public static void main (String[]args) {
+		
+		Integer a;
+		Integer b;
+		Integer opcion;
+		Integer resultado;
+		
+		Scanner teclado = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese su operacion /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dvidir");
+			
+			opcion = teclado.nextInt();
+			
+		}while (opcion > 1 && opcion > 4);
+			
+			System.out.println("Ingrese el primer numero");
+		    a=teclado.nextInt();
+
+			System.out.println("Ingrese el segundo numero");
+			b=teclado.nextInt();
+		
+		switch (opcion){
+			case (1):
+			resultado = a + b;
+			break;
+			case (2):
+			resultado = a - b;
+			break;
+			case (3):
+			resultado = a * b;
+			break;
+			case (4):
+			resultado = a / b;
+			break;
+		}
+			
+	
+
+			System.out.println("El resultado es " + resultado.toString());
+	}
+}
